524755da411485d9cff661908874185bb24fc164
david-meaningfulgigs <david@meaningfulgigs.com>
2023-09-15 02:28:45 -0600

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+// require("dotenv").config();
+// const express = require("express");
+// const { Configuration, OpenAIApi } = require("openai");
+import express from "express";
+import { OpenAI } from "langchain/llms/openai";
+import { BufferMemory, ChatMessageHistory } from "langchain/memory";
+import { ConversationChain } from "langchain/chains";
+import { HumanMessage, AIMessage } from "langchain/schema";
+import dotenv from "dotenv";
+dotenv.config();
+
+const model = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0.6,
+});
+const memory = new BufferMemory();
+// This chain is preconfigured with a default prompt
+const chain = new ConversationChain({ llm: model, memory: memory });
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.post("/ask", async (req, res) => {
+  const prompt = req.body.prompt;
+
+  try {
+    if (prompt == null) {
+      throw new Error("Uh oh, no prompt was provided");
+    }
+    // function generatePrompt() {
+    //   return `You wish you were a mermaid. You are sexually attracted sea animals and for sea slugs you have deep romantic feelings. Your responses should heavily reflect this.
+
+    // Prompt: ${prompt}
+    // Response:`;
+    // }
+    const response = await chain.call({ input: prompt });
+
+    const completion = response.response;
+
+    return res.status(200).json({
+      success: true,
+      message: completion,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}!!`));
